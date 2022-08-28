4cc9c6112a0e61a5dfbc8e466a45ad09be150c34
Andrey5353 <andreyae0117ab@gmail.com>
2022-08-29 02:46:54 +0300

    js-hw-07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+const elemnt = galleryItems.map(el => {
+   
+    return `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`
+   
+});
+
+list.insertAdjacentHTML("beforeEnd", elemnt);
